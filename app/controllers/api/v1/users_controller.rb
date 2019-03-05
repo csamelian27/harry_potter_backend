87d8ac39f0b0b0dcc44c670ab277b6326d124111
github username <github email address>
2019-03-05 17:09:17 -0500

    added update action to users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,9 +15,15 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    render json: @user
+  end
+
   private
   def user_params
-    params.permit(:name, :house)
+    params.permit(:name, :house, :pet_id, :broom_id, :wand_id)
   end
 
 end
