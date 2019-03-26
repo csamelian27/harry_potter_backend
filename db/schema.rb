5611df29b7f22c61d9c4e0201094c3f407bcb369
github username <github email address>
2019-03-26 18:08:59 -0400

    updated ruby and changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_07_144838) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "subject"
